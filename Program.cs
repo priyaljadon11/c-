0394c3a06eb04b64c6e4ac5a09af457ff25cc818
priyaljadon11 <102207727+priyaljadon11@users.noreply.github.com>
2022-03-24 12:49:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MultiCast
+{
+    public delegate void Calculation(int a, int b);
+    class Program
+    {
+        public static void Addition(int a,int b)
+        {
+            int result = a + b;
+            Console.WriteLine("Addition result: {0}", result);
+
+        }
+        public static void Multiplication(int a, int b)
+        {
+            int result = a * b;
+            Console.WriteLine("Multiplication result: {0}", result);
+
+        }
+        public static void Subtraction(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine("Subtraction result: {0}", result);
+
+        }
+        public static void Main(string[] args)
+        {
+            Calculation obj = new Calculation(Addition);
+            obj += Subtraction;
+            obj += Multiplication;
+
+            obj(30, 20);
+        }
+
+    }
+}
