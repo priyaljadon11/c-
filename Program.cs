20f847be56ae9969a8f6420ec73de6ab7df32e46
priyaljadon11 <102207727+priyaljadon11@users.noreply.github.com>
2022-03-24 12:47:10 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+
+ public delegate void Message(string Message);
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Message obj = new Message(Program.Hello);
+        obj("priyal");
+    }
+    public static void Hello(string strMessage)
+    {
+        Console.WriteLine("Print Message: {0}", strMessage);
+    }
+}
