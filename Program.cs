432f1845885e829e4180b9cb74a4c932e5003c2f
priyaljadon11 <102207727+priyaljadon11@users.noreply.github.com>
2022-03-24 12:40:22 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CallByReference
+{
+    class Program
+    {
+        public void cal1(ref int val)
+        {
+            val = val * val;
+            Console.WriteLine("inside the call by reference " + val);
+        }
+        static void Main(string[] args)
+        {
+            int val = 25;
+            Program obj = new Program();
+            Console.WriteLine("Before calling by reference " + val);
+            obj.cal1(ref val);
+            Console.WriteLine("After callin by reference " + val);
+
+        }
+    }
+}
