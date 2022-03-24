43626c14ac6a7b383656e7947067409ba420dc01
priyaljadon11 <102207727+priyaljadon11@users.noreply.github.com>
2022-03-24 12:54:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Anonymous
+{
+    class Program { 
+
+        delegate void Anonyms(int a);
+        static void Main(string[] args)
+        {
+            Anonyms obj = delegate (int a)
+            {
+                a += 10;
+                Console.WriteLine(a);
+            };
+            obj(5);
+        }
+    
+
+    }
+}
